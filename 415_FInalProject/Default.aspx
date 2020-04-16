72a2b403eba6f821cd450f763a9b788cdc97d314
Daniel Vogt <danielharlowvogt@gmail.com>
2020-04-16 12:13:09 -0600

    begin

---
+++ 415_FInalProject/Default.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_415_FInalProject.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Bike Buyers Prediction Calculator</h1>
+        </div>
+
+
+        <table>
+            <tr>
+                <td>Marital Status:</td>
+                <td><asp:RadioButtonList ID="rblMaritalStatus" runat="server">
+                    <asp:ListItem>Married</asp:ListItem>
+                    <asp:ListItem>Single</asp:ListItem>
+                </asp:RadioButtonList></td>
+            </tr>
+            <tr>
+                <td>Gender:</td>
+                <td><asp:RadioButtonList ID="rblGender" runat="server">
+                    <asp:ListItem>Male</asp:ListItem>
+                    <asp:ListItem>Female</asp:ListItem>
+                </asp:RadioButtonList></td>
+            </tr>
+            <tr>
+                <td>Income:</td>
+                <td>
+                    <asp:TextBox ID="txtIncome" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Children:</td>
+                <td>
+                    <asp:DropDownList ID="ddlChildren" runat="server">
+                        <asp:ListItem Value="1">1</asp:ListItem>
+                        <asp:ListItem Value="2">2</asp:ListItem>
+                        <asp:ListItem Value="3">3</asp:ListItem>
+                        <asp:ListItem Value="4">4</asp:ListItem>
+                        <asp:ListItem Value="5">5</asp:ListItem>
+                        <asp:ListItem Value="6">6+</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td>Cars:</td>
+                <td><asp:TextBox ID="txtCars" runat="server"></asp:TextBox></td>
+                
+            </tr>
+            <tr>
+                <td>Age:</td>
+                <td><asp:TextBox ID="txtAge" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <asp:LinkButton ID="lbPredict" runat="server" OnClick="lbPredict_Click">Predict</asp:LinkButton>
+                </td>
+            </tr>
+
+        </table>
+        
+        
+        <asp:Label ID="lblResults" runat="server" Font-Size="Larger"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ 415_FInalProject/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+namespace _415_FInalProject
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void lbPredict_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient("https://ussouthcentral.services.azureml.net/workspaces/2abd23f891284eb98f5356e46b5cb743/services/25cae0be6e7a42f383bc8c9d22845432/execute?api-version=2.0&details=true");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", "Bearer 4DLSz1Ulm2VrIAxzfZ58KhloOLnbfCe50va1g+ya4AXdfk3/BeZ154EenhykPeWHUo47r/icSR06rYf8zh5ppg==");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json,text/plain", "{\r\n  \"Inputs\": {\r\n    \"input1\": {\r\n      \"ColumnNames\": [\r\n        \"Marital Status\",\r\n        \"Gender\",\r\n        \"Income\",\r\n        \"Children\",\r\n        \"Cars\",\r\n        \"Age\",\r\n        \"Education\",\r\n        \"Occupation\",\r\n        \"Home Owner\",\r\n        \"Commute Distance\",\r\n        \"Region\",\r\n        \"Purchased Bike\"\r\n      ],\r\n      \"Values\": [\r\n        [\r\n          \"" + rblMaritalStatus.SelectedItem.Text + "\",\r\n          \"" + rblGender.SelectedItem.Text + "\",\r\n          \"" + txtIncome.Text + "\",\r\n          \"" + ddlChildren.SelectedValue + "\",\r\n          \"" + txtCars.Text + "\",\r\n          \"" + txtAge.Text + "\",\r\n          \"college\",\r\n          \"farmer\",\r\n          \"yes\",\r\n          \"33\",\r\n          \"alberta\",\r\n          \"yes\"\r\n        ],\r\n      ]\r\n    }\r\n  },\r\n  \"GlobalParameters\": {}\r\n}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            var results = JObject.Parse(response.Content);
+            string prediction = results["Results"]["output1"]["value"]["Values"].ToString();
+            prediction = prediction.Replace("[", "").Replace("]", "").Replace("\"", "");
+
+            //Get response to front page
+            lblResults.Text = prediction;
+
+            Console.WriteLine(response.Content);
+        }
+    }
+}
